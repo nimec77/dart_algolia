13f377f5484712c008da92241c65057ab95dd365
comrade77 <nimec77@gmail.com>
2021-06-11 14:42:31 +0700

    Add public factory constructor to error.dart

--- lib/src/error.dart
+++ lib/src/error.dart
@@ -4,6 +4,8 @@ class AlgoliaError {
   final Map _message;
   final int _statusCode;
 
+  factory AlgoliaError.fromResponse(Map error, int statusCode) => AlgoliaError._(error, statusCode);
+
   AlgoliaError._(Map error, int statusCode)
       : _message = error,
         _statusCode = statusCode;
